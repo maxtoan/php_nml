411d51144e7d68d2181e61c6b33b8d2686e48384
Travis <travis@travis-ci.org>
2015-07-14 18:40:44 +0000

    API updated

---
+++ api/api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","NelsonMartell\\Collections\\Collection"],["c","NelsonMartell\\Collections\\CollectionIterator"],["c","NelsonMartell\\Collections\\ICollection"],["c","NelsonMartell\\Collections\\IList"],["c","NelsonMartell\\Extensions\\String"],["c","NelsonMartell\\IComparable"],["c","NelsonMartell\\IEquatable"],["c","NelsonMartell\\IntString"],["c","NelsonMartell\\Object"],["c","NelsonMartell\\PropertiesHandler"],["c","NelsonMartell\\Type"],["c","NelsonMartell\\Utilities\\Asset"],["c","NelsonMartell\\Utilities\\UnitTesting\\Assert"],["c","NelsonMartell\\Version"],["c","NelsonMartell\\VersionComponent"],["f","nml_msg()"],["f","nml_nmsg()"],["f","typeof()"]];
